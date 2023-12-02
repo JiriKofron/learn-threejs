a07de0ca86689b7a213c5b86c1f3855dc496ef14
Jiří Kofroň <kofron.jiri@gmail.com>
2023-12-02 22:54:21 +0100

    initial WIP ball

---
+++ pages/ball/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import * as THREE from "three";
+import gsap from 'gsap'
+
+const scene = new THREE.Scene()
+const renderrer = new THREE.WebGLRenderer
+const init = () => {
+  gsap.ticker
+}
+
+onMounted(() => {
+
+})
+</script>
+
+<template>
+  <main>
+    <h1>Ball</h1>
+
+    <canvas
+      id="c"
+      ref="threeCanvas"
+      width="500"
+      height="500"
+    />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
